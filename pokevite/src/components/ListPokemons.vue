3eae9e90ca3b139d643b21689da1505ef0fb6516
brenobarroso <breno.barroso@pagcerto.com.br>
2023-08-25 17:07:30 -0300

    pokemons are now with css

--- pokevite/src/components/ListPokemons.vue
+++ pokevite/src/components/ListPokemons.vue
@@ -7,7 +7,7 @@ const pokemon = defineProps(["name", "urlBaseSVG"])
 <template>
   
 <div class="col-4">
-    <div class="card p-2 mb-3">
+    <div class="card p-2 mb-3 cardListPokemon">
         <p class="text-center">{{ pokemon.name }}</p>
         <img :src="urlBaseSVG" class="card-img-top" alt="..." height="80">
     </div>
@@ -15,8 +15,21 @@ const pokemon = defineProps(["name", "urlBaseSVG"])
 
 </template>
 
-<style>
+<style scoped>
+.cardListPokemon{
+    background: rgb(238,174,202);
+    background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+    cursor: pointer;
+}
 
+.cardListPokemon:hover{
+    background: rgb(238,174,174);
+    background: radial-gradient(circle, rgba(238,174,174,1) 0%, rgba(148,233,205,1) 100%);
+    cursor: pointer;
+}
 
+.cardListPokemon img:hover{
+    padding: .1rem;
+}
 
 </style>
